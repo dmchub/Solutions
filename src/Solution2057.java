b8a2b52d76faf276e114cf366dd22576dee02bdc
dmchub <37676729+dmchub@users.noreply.github.com>
2020-06-15 00:11:46 +0300

    Solution with using TreeMap

--- src/Solution2057.java
+++ src/Solution2057.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Solution2057 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> ls = new ArrayList<>();
-        TreeMap<String, Integer> treeMap = new TreeMap<>();
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        List<Integer> listOut = new ArrayList();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
@@ -14,15 +14,34 @@ public class Solution2057 {
             String str = br.readLine();
             String[] strTemp = str.split(" ", -1);
             List<String> list = new ArrayList<>(Arrays.asList(strTemp));
+
             if(Integer.parseInt(list.get(0))==1){
                 int a = Integer.parseInt(list.get(1));
-                ls.add(a);
+                if(treeMap.containsKey(a)){
+                    treeMap.put(a, treeMap.get(a) + 1);
+                }else {
+                    treeMap.put(a, 1);
+                }
+                // System.out.println(treeMap);
+                // System.out.println(treeMap.firstKey());
+                // System.out.println(treeMap.get(treeMap.firstKey()));
             }
-            if(Integer.parseInt(list.get(0))==2){
-                System.out.println(Collections.min(ls));
-                // ls.remove(minValue);
 
+            if(Integer.parseInt(list.get(0))==2){
+                // System.out.println(treeMap.firstKey());
+                listOut.add(treeMap.firstKey());
+                if(treeMap.get(treeMap.firstKey()) >= 1){
+                    treeMap.put(treeMap.firstKey(), treeMap.get(treeMap.firstKey()) - 1);
+                }
+                if(treeMap.get(treeMap.firstKey()) == 0){
+                    treeMap.remove(treeMap.firstKey());
+                }
+                // System.out.println(treeMap);
             }
+        }
+
+        for(int e : listOut){
+            System.out.println(e);
         }
     }
 }
